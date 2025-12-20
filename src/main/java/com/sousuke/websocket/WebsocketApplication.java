1e28cc0e9c10244e3c3331b28ccb19ccb799793e
Naing Lynn Aung <nl.a@mit.com.mm>
2025-12-21 02:38:18 +0630

    websocket spring

---
+++ src/main/java/com/sousuke/websocket/WebsocketApplication.java
@@ -0,0 +1,24 @@
+package com.sousuke.websocket;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+
+    @Bean
+    public ApplicationRunner hello(){
+        return args ->
+        {
+            System.out.println("How are you");
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sousuke/websocket/config/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.sousuke.websocket.config;
+
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+}
